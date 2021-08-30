7a4117a54a4d42292f077e65a9eedfc04b6c38b8
DAVTCHX <davtech@gmail.com>
2021-08-30 03:40:56 -0400

    react Router DOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from "react";
-import BtcMainPage from './btcMainPage/BtcMainPage';
-
-
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import BtcMainPage from "./btcMainPage/BtcMainPage";
+import Checkout from "./checkout/Checkout";
+import SignIn from "./sign-in/SignIn";
+import SignUp from "./sign-up/SignUp";
+import Dashboard from "./dashboard/Dashboard";
 
 function App() {
   return (
-      <>
-      <BtcMainPage />
-      </>
+    <BrowserRouter>
+      <Route exact path="/" component={BtcMainPage} />
+      <Route path="/plans" component={SignIn} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/aboutus" component={Checkout} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/contactus" component={SignIn} />
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
